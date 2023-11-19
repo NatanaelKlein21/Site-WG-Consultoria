efbd2a86e0c0d873158ec0c7f679d965d638a691
Natanael Klein <89701520+NatanaelKlein21@users.noreply.github.com>
2023-11-19 04:37:51 -0300

    versao 1 - 19/11/2023

---
+++ script.js
@@ -0,0 +1,9 @@
+// whatsapp
+function redirecionarParaWhatsApp() {
+    window.open('https://api.whatsapp.com/message/GO2TMND4TM6EJ1?autoload=1&app_absent=0', '_blank');
+}
+
+// instagram
+function redirecionarParaInstagram() {
+    window.open('https://www.instagram.com/consultoria.wg/', '_blank');
+}
