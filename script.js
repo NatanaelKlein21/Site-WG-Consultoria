ec494801d4abd935dc72cad3f912230864e616eb
Natanael Klein <89701520+NatanaelKlein21@users.noreply.github.com>
2023-11-26 05:22:26 -0300

    versão 26/11/2023

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function redirecionarParaWhatsApp() {
 function redirecionarParaInstagram() {
     window.open('https://www.instagram.com/consultoria.wg/', '_blank');
 }
+
+
+var swiper = new Swiper('.slide-content', {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3
+        }
+    },
+ });
